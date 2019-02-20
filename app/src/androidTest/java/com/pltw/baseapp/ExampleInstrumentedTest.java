5d8823447ddf98a0d09bf3623d8edd9c734d62e3
Shivank Joshi <47608238+glokk9@users.noreply.github.com>
2019-02-20 11:47:48 -0500

    Revert "shivank commit not feeling so good"
    This reverts commit 4b7b57d7a158ae896ccf4421700b90c205bca462.

--- app/src/androidTest/java/com/pltw/baseapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/pltw/baseapp/ExampleInstrumentedTest.java
@@ -20,7 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-        //hi its emma and shivank
+        //hi its emma
 
         assertEquals("com.pltw.baseapp", appContext.getPackageName());
     }
